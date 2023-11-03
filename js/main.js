451f6ee2472642894d439603fb8adfb3f6a7af61
waqas <waqas.hanif@sixlogics.com>
2023-11-03 18:53:06 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,74 @@
-$('.slider-products').slick({
-    slidesToShow: auto,
-    variableWidth: true,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    infinite: false,
-    asNavFor: '.products-nav'
-  });
-  $('.products-nav').slick({
-    vertical: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    infinite: false,
-    asNavFor: '.slider-products',
-    dots: false,
-    centerMode: true,
-    focusOnSelect: true,
+// $('.slider-products').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     infinite: false,
+//     asNavFor: '.products-nav'
+//   });
+//   $('.products-nav').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     infinite: false,
+//     centerPadding: '0px',
+//     //asNavFor: '.slider-products',
+//     dots: false,
+//     centerMode: true,
+//     focusOnSelect: true,
+//     arrows: true,
     
+//   });
+
+
+
+
+
+
+  const sliderTop = new Swiper(".gallery-top", {
+    spaceBetween: 0,
+    coverflowEffect: {
+      rotate: 60,
+      slideShadows: false
+    },
+    centeredSlides: true,
+    initialSlide: 1,
+    breakpoints: {
+      300: {
+        slidesPerView: 1
+      },
+      1100: {
+        slidesPerView: 2
+      },
+      1650: {
+        slidesPerView: 3
+      }
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    loop: false,
+    loopedSlides: 3,
+    slideToClickedSlide: true
   });
+  
+  const sliderBottom = new Swiper(".gallery-bottom", {
+    initialSlide: 1,
+    centeredSlides: true,
+    effect: "fade",
+    
+    loop: false,
+    loopedSlides: 3
+  });
+  
+  sliderTop.controller.control = sliderBottom;
+  sliderBottom.controller.control = sliderTop;
+  
+  // sliderTop.on("slideChange", function () {
+  //   sliderBottom.slideTo(sliderTop.activeIndex);
+  // });
+  
+  // sliderBottom.on("slideChange", function () {
+  //   sliderTop.slideTo(sliderBottom.activeIndex);
+  // });
+  
